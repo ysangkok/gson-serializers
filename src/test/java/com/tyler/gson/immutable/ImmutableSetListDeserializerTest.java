bb594bfa8e58fbe56f249a4d249035a96e906b0d
Janus <ysangkok@gmail.com>
2016-06-05 16:58:31 +0200

    complete example to match code in bug report

--- src/test/java/com/tyler/gson/immutable/ImmutableSetListDeserializerTest.java
+++ src/test/java/com/tyler/gson/immutable/ImmutableSetListDeserializerTest.java
@@ -1,4 +1,5 @@
 package com.tyler.gson.immutable;
+import com.google.gson.reflect.TypeToken;
 
 import static org.junit.Assert.assertThat;
 
@@ -6,42 +7,111 @@ import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
 import org.hamcrest.core.Is;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableList;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+import java.lang.reflect.Type;
+
+class AnotherType {
+    int a;
+    String b;
+    AnotherType(int a, String b) {
+        if (b == null) throw new RuntimeException("b is null");
+        this.a = a;
+        this.b = b;
+    }
+}
+
+class AnotherTypeDeserializer implements JsonDeserializer<AnotherType> {
+    @Override
+    public AnotherType deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jobject = (JsonObject) json;
+
+        return new AnotherType(
+                jobject.get("a").getAsInt(),
+                jobject.get("b").getAsString());
+    }
+}
+
+class MyData {
+        int x;
+        String y;
+	ImmutableSet<AnotherType> zs;
+	MyData(int x, String y, ImmutableSet<AnotherType> zs) {
+		this.x = x;
+		this.y = y;
+		this.zs = zs;
+	}
+}
+
+class MyDataDeserializer implements JsonDeserializer<MyData> {
+	@Override
+	public MyData deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+		JsonObject o = (JsonObject) json;
+
+		Type collectionType = new TypeToken<Collection<AnotherType>>(){}.getType();
+
+		ImmutableSet.Builder<AnotherType> setBuilder = new ImmutableSet.Builder<AnotherType>();
+		Collection<AnotherType> col = context.deserialize(o.get("zs"), collectionType);
+		for (AnotherType i: col) {
+			assertThat(i, IsInstanceOf.any(AnotherType.class));
+			setBuilder.add(i);
+		}
+		ImmutableSet<AnotherType> set = setBuilder.build();
+
+		int x = o.get("x").getAsInt();
+		String y = o.get("y").getAsString();
+
+		return new MyData(x, y, set);
+	}
+}
+
+class EmbeddedInterface {
+	ImmutableList<MyData> list;
+	EmbeddedInterface(ImmutableList<MyData> s){
+		this.list = s;
+	}
+}
+
+class EmbeddedInterfaceDeserializer implements JsonDeserializer<EmbeddedInterface> {
+	@Override
+	public EmbeddedInterface deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+		Type collectionType = new TypeToken<Collection<MyData>>(){}.getType();
+		Collection<MyData> col = context.deserialize(json, collectionType);
+		return new EmbeddedInterface(ImmutableList.copyOf(col));
+	}
+}
 
 public class ImmutableSetListDeserializerTest {
-	private final ImmutableSet<String> testSet = ImmutableSet.of( "cc" );
-	private final ImmutableList<ImmutableSet<String>> testSetList = ImmutableList.of( testSet );
-	private final String jsonSet = "[ \"cc\" ]";
-	private final String jsonSetList = "{ \"list\": [ " + jsonSet + " ] }";
-	private final EmbeddedInterface tester = new GsonBuilder().registerTypeAdapter(ImmutableList.class, new ImmutableListDeserializer())
-	                .registerTypeAdapter(ImmutableSet.class, new ImmutableSetDeserializer()).create().fromJson( jsonSetList, EmbeddedInterface.class );
+	private Gson gson = new GsonBuilder().registerTypeAdapter(ImmutableList.class, new ImmutableListDeserializer())
+		.registerTypeAdapter(ImmutableSet.class, new ImmutableSetDeserializer())
+		.registerTypeAdapter(AnotherType.class, new AnotherTypeDeserializer())
+		.registerTypeAdapter(EmbeddedInterface.class, new EmbeddedInterfaceDeserializer())
+		.registerTypeAdapter(MyData.class, new MyDataDeserializer())
+		.create();
 
 	@Test
-	public void testEmbeddedEquals() {
-		
-		ImmutableList.Builder<ImmutableSet<String>> listBuilder = new ImmutableList.Builder<ImmutableSet<String>>();
-		for (int i = 0; i<tester.list.size(); i++)
-			listBuilder.add(ImmutableSet.copyOf(tester.list.get(i)));
-		ImmutableList<ImmutableSet<String>> immuList = listBuilder.build();
-		assertThat( immuList, Is.is( testSetList ));
+	public void testDeserializeAnotherType() {
+		AnotherType i = gson.fromJson("{\"a\":12,\"b\":\"hij\"}", AnotherType.class);
+		assertThat(i.a, Is.is(12));
+		assertThat(i.b, Is.is("hij"));
 	}
 
-	@Test(expected=ClassCastException.class) // the embedded set is actually an ArrayList and once we iterate it gets casted to ImmutableSet
-	public void testEmbeddedException() {
-		tester.list.get(0).iterator();
+	@Test
+	public void testDeserializeMyData() {
+		MyData i = gson.fromJson("{\"x\": 512, \"y\": \"jones\", \"zs\": [{\"a\":42,\"b\":\"hi\"}]}", MyData.class);
+		assertThat(i.zs.iterator().next().a, Is.is(42));
 	}
 
-	private static final class EmbeddedInterface { 
-		private final ImmutableList<ImmutableSet<String>> list;
-		
-		private EmbeddedInterface(){
-			this.list = ImmutableList.of();
-		}		
+	@Test
+	public void testDeserializeList() {
+		EmbeddedInterface i = gson.fromJson("[{\"x\": 100, \"y\": \"james\", \"zs\": [{\"a\":-5,\"b\":\"hallo\"}]}]", EmbeddedInterface.class);
+		assertThat(i.list.get(0).zs.iterator().next().a, Is.is(-5));
 	}
 }
