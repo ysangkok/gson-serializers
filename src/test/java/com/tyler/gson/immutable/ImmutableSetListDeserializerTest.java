d618582fa03fa6aa9ff0e3b2639c6d822e85c9f8
Janus <ysangkok@gmail.com>
2016-06-05 17:08:32 +0200

    format code, organize imports, rename class

--- src/test/java/com/tyler/gson/immutable/ImmutableSetListDeserializerTest.java
+++ src/test/java/com/tyler/gson/immutable/ImmutableSetListDeserializerTest.java
@@ -1,34 +1,32 @@
 package com.tyler.gson.immutable;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
-
-import static org.junit.Assert.assertThat;
-
-import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.lang.reflect.Type;
 import java.util.Collection;
-
 import org.hamcrest.core.Is;
 import org.hamcrest.core.IsInstanceOf;
+import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.ImmutableList;
-import com.google.gson.*;
-import java.lang.reflect.Type;
-
 class AnotherType {
+
     int a;
     String b;
+
     AnotherType(int a, String b) {
-        if (b == null) throw new RuntimeException("b is null");
+        if (b == null) {
+            throw new RuntimeException("b is null");
+        }
         this.a = a;
         this.b = b;
     }
 }
 
 class AnotherTypeDeserializer implements JsonDeserializer<AnotherType> {
+
     @Override
     public AnotherType deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
         JsonObject jobject = (JsonObject) json;
@@ -40,78 +38,87 @@ class AnotherTypeDeserializer implements JsonDeserializer<AnotherType> {
 }
 
 class MyData {
-        int x;
-        String y;
-	ImmutableSet<AnotherType> zs;
-	MyData(int x, String y, ImmutableSet<AnotherType> zs) {
-		this.x = x;
-		this.y = y;
-		this.zs = zs;
-	}
+
+    int x;
+    String y;
+    ImmutableSet<AnotherType> zs;
+
+    MyData(int x, String y, ImmutableSet<AnotherType> zs) {
+        this.x = x;
+        this.y = y;
+        this.zs = zs;
+    }
 }
 
 class MyDataDeserializer implements JsonDeserializer<MyData> {
-	@Override
-	public MyData deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-		JsonObject o = (JsonObject) json;
 
-		Type collectionType = new TypeToken<Collection<AnotherType>>(){}.getType();
+    @Override
+    public MyData deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject o = (JsonObject) json;
 
-		ImmutableSet.Builder<AnotherType> setBuilder = new ImmutableSet.Builder<AnotherType>();
-		Collection<AnotherType> col = context.deserialize(o.get("zs"), collectionType);
-		for (AnotherType i: col) {
-			assertThat(i, IsInstanceOf.any(AnotherType.class));
-			setBuilder.add(i);
-		}
-		ImmutableSet<AnotherType> set = setBuilder.build();
+        Type collectionType = new TypeToken<Collection<AnotherType>>() {
+        }.getType();
 
-		int x = o.get("x").getAsInt();
-		String y = o.get("y").getAsString();
+        ImmutableSet.Builder<AnotherType> setBuilder = new ImmutableSet.Builder<AnotherType>();
+        Collection<AnotherType> col = context.deserialize(o.get("zs"), collectionType);
+        for (AnotherType i : col) {
+            assertThat(i, IsInstanceOf.any(AnotherType.class));
+            setBuilder.add(i);
+        }
+        ImmutableSet<AnotherType> set = setBuilder.build();
 
-		return new MyData(x, y, set);
-	}
+        int x = o.get("x").getAsInt();
+        String y = o.get("y").getAsString();
+
+        return new MyData(x, y, set);
+    }
 }
 
-class EmbeddedInterface {
-	ImmutableList<MyData> list;
-	EmbeddedInterface(ImmutableList<MyData> s){
-		this.list = s;
-	}
+class ListWrapper {
+
+    ImmutableList<MyData> list;
+
+    ListWrapper(ImmutableList<MyData> s) {
+        this.list = s;
+    }
 }
 
-class EmbeddedInterfaceDeserializer implements JsonDeserializer<EmbeddedInterface> {
-	@Override
-	public EmbeddedInterface deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-		Type collectionType = new TypeToken<Collection<MyData>>(){}.getType();
-		Collection<MyData> col = context.deserialize(json, collectionType);
-		return new EmbeddedInterface(ImmutableList.copyOf(col));
-	}
+class ListWrapperDeserializer implements JsonDeserializer<ListWrapper> {
+
+    @Override
+    public ListWrapper deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        Type collectionType = new TypeToken<Collection<MyData>>() {
+        }.getType();
+        Collection<MyData> col = context.deserialize(json, collectionType);
+        return new ListWrapper(ImmutableList.copyOf(col));
+    }
 }
 
 public class ImmutableSetListDeserializerTest {
-	private Gson gson = new GsonBuilder().registerTypeAdapter(ImmutableList.class, new ImmutableListDeserializer())
-		.registerTypeAdapter(ImmutableSet.class, new ImmutableSetDeserializer())
-		.registerTypeAdapter(AnotherType.class, new AnotherTypeDeserializer())
-		.registerTypeAdapter(EmbeddedInterface.class, new EmbeddedInterfaceDeserializer())
-		.registerTypeAdapter(MyData.class, new MyDataDeserializer())
-		.create();
 
-	@Test
-	public void testDeserializeAnotherType() {
-		AnotherType i = gson.fromJson("{\"a\":12,\"b\":\"hij\"}", AnotherType.class);
-		assertThat(i.a, Is.is(12));
-		assertThat(i.b, Is.is("hij"));
-	}
+    private Gson gson = new GsonBuilder().registerTypeAdapter(ImmutableList.class, new ImmutableListDeserializer())
+            .registerTypeAdapter(ImmutableSet.class, new ImmutableSetDeserializer())
+            .registerTypeAdapter(AnotherType.class, new AnotherTypeDeserializer())
+            .registerTypeAdapter(ListWrapper.class, new ListWrapperDeserializer())
+            .registerTypeAdapter(MyData.class, new MyDataDeserializer())
+            .create();
 
-	@Test
-	public void testDeserializeMyData() {
-		MyData i = gson.fromJson("{\"x\": 512, \"y\": \"jones\", \"zs\": [{\"a\":42,\"b\":\"hi\"}]}", MyData.class);
-		assertThat(i.zs.iterator().next().a, Is.is(42));
-	}
+    @Test
+    public void testDeserializeAnotherType() {
+        AnotherType i = gson.fromJson("{\"a\":12,\"b\":\"hij\"}", AnotherType.class);
+        assertThat(i.a, Is.is(12));
+        assertThat(i.b, Is.is("hij"));
+    }
 
-	@Test
-	public void testDeserializeList() {
-		EmbeddedInterface i = gson.fromJson("[{\"x\": 100, \"y\": \"james\", \"zs\": [{\"a\":-5,\"b\":\"hallo\"}]}]", EmbeddedInterface.class);
-		assertThat(i.list.get(0).zs.iterator().next().a, Is.is(-5));
-	}
+    @Test
+    public void testDeserializeMyData() {
+        MyData i = gson.fromJson("{\"x\": 512, \"y\": \"jones\", \"zs\": [{\"a\":42,\"b\":\"hi\"}]}", MyData.class);
+        assertThat(i.zs.iterator().next().a, Is.is(42));
+    }
+
+    @Test
+    public void testDeserializeList() {
+        ListWrapper i = gson.fromJson("[{\"x\": 100, \"y\": \"james\", \"zs\": [{\"a\":-5,\"b\":\"hallo\"}]}]", ListWrapper.class);
+        assertThat(i.list.get(0).zs.iterator().next().a, Is.is(-5));
+    }
 }
